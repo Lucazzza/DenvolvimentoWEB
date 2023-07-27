44d4412559280d3548c0073c1b7daf618d1bce74
Lucazzza <lucas.souza5294@gmail.com>
2023-07-27 10:31:37 -0300

    TokenTravel ATualizado CSS e Rotas traçadas no mapa.II

--- TokenTravel/usuario/backends.py
+++ TokenTravel/usuario/backends.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth import get_user_model
 from django.db.models import Q
-
 class PassageiroBackend(BaseBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         UserModel = get_user_model()
@@ -11,15 +10,12 @@ class PassageiroBackend(BaseBackend):
             return None
         if user.check_password(password):
             return user
-
     def get_user(self, user_id):
         UserModel = get_user_model()
         try:
             return UserModel.objects.get(pk=user_id, is_active=True, is_passageiro=True)
         except UserModel.DoesNotExist:
             return None
-
-
 class MotoristaBackend(BaseBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
         UserModel = get_user_model()
@@ -29,7 +25,6 @@ class MotoristaBackend(BaseBackend):
             return None
         if user.check_password(password):
             return user
-
     def get_user(self, user_id):
         UserModel = get_user_model()
         try:
--- TokenTravel/usuario/forms.py
+++ TokenTravel/usuario/forms.py
@@ -1,14 +1,9 @@
 from django import forms
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
-from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.validators import RegexValidator
 from .models import Passageiro, Motorista, RotaViagem, DiaSemana
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
-
-# Passageiros
-
 class PassageiroFormPadrao(UserCreationForm):
     email = forms.EmailField(required=True)
     nome_completo = forms.CharField(max_length=180, label='Nome Completo')
@@ -23,20 +18,15 @@ class PassageiroFormPadrao(UserCreationForm):
     bairro = forms.CharField(max_length=50)
     cidade = forms.CharField(max_length=50)
     estado = forms.CharField(max_length=2)
-
     class Meta:
         model = User and Passageiro
         fields = ('nome_completo', 'nome_preferencia', 'cpf', 'telefone', 'cep', 'logradouro', 'numero_residencia', 'bairro', 'cidade', 'estado', 'username', 'email')
-
     def save(self, commit=True):
         usuario = super().save(commit=False)
         usuario.email = self.cleaned_data['email']
         if commit:
             usuario.save()
         return usuario
-
-# Motoritas
-
 class MotoristaFormPadrao(UserCreationForm):
     email = forms.EmailField(required=True)
     nome_completo = forms.CharField(max_length=180, label='Nome Completo')
@@ -55,20 +45,15 @@ class MotoristaFormPadrao(UserCreationForm):
     placa = forms.CharField(max_length=7)
     renavam = forms.CharField(max_length=11)
     chassi = forms.CharField(max_length=17)
-
     class Meta:
         model = User and Motorista
         fields = ('nome_completo', 'nome_preferencia', 'cpf', 'telefone', 'cep', 'logradouro', 'numero_residencia', 'bairro', 'cidade', 'estado', 'cnh', 'placa', 'renavam', 'chassi', 'username', 'email')
-
     def save(self, commit=True):
         motorista = super().save(commit=False)
         motorista.email = self.cleaned_data['email']
         if commit:
             motorista.save()
         return motorista
-
-# Continuando Usuário
-
 class PassageiroFormPersonalizado(forms.ModelForm):
     is_superuser = forms.BooleanField(
         label=_('Administrador'),
@@ -76,11 +61,9 @@ class PassageiroFormPersonalizado(forms.ModelForm):
         initial=False,
         required=False
     )
-
     class Meta:
         model = User and Passageiro
         fields = '__all__'
-
     def __init__(self, *args, **kwargs):
         super(PassageiroFormPersonalizado, self).__init__(*args, **kwargs)
         if self.instance.is_superuser:
@@ -88,10 +71,6 @@ class PassageiroFormPersonalizado(forms.ModelForm):
         else:
             self.fields['is_superuser'].initial = False
         self.fields['is_superuser'].label = _('Administrador')
-
-
-# Continuando Motorista
-
 class MotoristaFormPersonalizado(forms.ModelForm):
     is_superuser = forms.BooleanField(
         label=_('Administrador'),
@@ -99,11 +78,9 @@ class MotoristaFormPersonalizado(forms.ModelForm):
         initial=False,
         required=False
     )
-
     class Meta:
         model = User and Motorista
         fields = '__all__'
-
     def __init__(self, *args, **kwargs):
         super(MotoristaFormPersonalizado, self).__init__(*args, **kwargs)
         if self.instance.is_superuser:
@@ -111,8 +88,6 @@ class MotoristaFormPersonalizado(forms.ModelForm):
         else:
             self.fields['is_superuser'].initial = False
         self.fields['is_superuser'].label = _('Administrador')
-
-
 class LoginForm(AuthenticationForm):
     email = forms.CharField(widget=forms.TextInput(attrs={'autofocus': True, 'type': 'email'}))
     password = forms.CharField(
@@ -120,22 +95,8 @@ class LoginForm(AuthenticationForm):
         strip=False,
         widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
     )
-
-# METODO QUE FUNFA
-
-from django import forms
-from .models import RotaViagem, DiaSemana
-
-
 class RotaViagemForm(forms.ModelForm):
     dias_semana = forms.MultipleChoiceField(choices=DiaSemana.DIAS_SEMANA_CHOICES, widget=forms.CheckboxSelectMultiple)
-
-    # class Meta:
-    #     model = RotaViagem
-    #     fields = ['origem', 'destino', 'preco', 'horario_ida', 'horario_volta', 'dias_semana']
-
     class Meta:
         model = RotaViagem
         fields = ['origem', 'destino', 'preco', 'horario_ida', 'dias_semana', 'tipo_veiculo']
-
-
